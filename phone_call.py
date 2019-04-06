030d2fae637a55e4746cd5c44437c3e8021d65ef
rand0musername <njov996@gmail.com>
2019-04-06 15:13:55 +0200

    Add roaming

--- phone_call.py
+++ phone_call.py
@@ -1,18 +1,19 @@
 class PhoneCall():
     allowed_keys = [
         'id', # int
-        'id_called' # int, id onoga koga smo pozvali
-        'datetime' # datetime
-        'orig_op_country' # one-hot vec drzave (isti!)
-        'transm_op_country' # one-hot vec drzave (isti!)
-        'recv_op_country' # one-hot vec drzave (isti!)
-        'dest_op_country' # one-hot vec drzave (isti!)
-        'tocs' # one-hot vec duzine 4? (FMNO, wholesale, EasyConnect, DSP)
-        'call_duration' # int (sekunde)
-        'setup_duration' # int (sekunde)
-        'answered' # int 0/1
-        'status_cat' # one-hot vec duzine 3? (network error, sub action, no error)
-        'status_name' # string??? videti koji su pa mozda one-hot !!!
+        'id_called', # int, id onoga koga smo pozvali
+        'datetime', # datetime
+        'orig_op_country', # one-hot vec drzave (isti!)
+        'transm_op_country', # one-hot vec drzave (isti!)
+        'recv_op_country', # one-hot vec drzave (isti!)
+        'dest_op_country', # one-hot vec drzave (isti!)
+        'tocs', # one-hot vec duzine 4? (FMNO, wholesale, EasyConnect, DSP)
+        'roaming', # int 0/1 da li je roaming: FMNO && orig_op != transm_op
+        'call_duration', # int (sekunde)
+        'setup_duration', # int (sekunde)
+        'answered', # int 0/1
+        'status_cat', # one-hot vec duzine 3? (network error, sub action, no error)
+        'status_name', # string??? videti koji su pa mozda one-hot !!!
         'release_dir' # string??? moze realno da bude one-hot !!!
     ]
 
